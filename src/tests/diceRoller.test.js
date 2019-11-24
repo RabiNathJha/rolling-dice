7b378b181ff068d6fdde0f0921252518d2ecb1c6
Rabinath Jha <rabinath.jha@datagrokr.com>
2019-11-25 00:52:55 +0530

    test: added test case to test randomness using chi-squared-test

---
+++ src/tests/diceRoller.test.js
@@ -0,0 +1,20 @@
+import chiSquaredTest from 'chi-squared-test';
+import prng from '../prng';
+
+describe('Testing dice roller randomness', () => {
+    it('testing randomness', () => {
+        const min = 1;
+        const max = 20;
+
+        const expected = Array(5).fill(0).map(() => prng(min, max));
+
+        const observed = Array(5).fill(0).map(() => prng(min, max));
+
+        const reduction = 1;
+
+        const {probability} = chiSquaredTest(observed, expected, reduction);
+        
+        expect(probability < 0.01).toEqual(true)
+        
+    })
+})
