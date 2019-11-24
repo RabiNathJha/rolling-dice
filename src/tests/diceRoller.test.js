8406fc58da39d12e0fe8d5397c4bc0b20d5bc512
Rabinath Jha <rabinath.jha@datagrokr.com>
2019-11-25 01:32:51 +0530

    test: updated test case range

--- src/tests/diceRoller.test.js
+++ src/tests/diceRoller.test.js
@@ -6,15 +6,15 @@ describe('Testing dice roller randomness', () => {
         const min = 1;
         const max = 20;
 
-        const expected = Array(5).fill(0).map(() => prng(min, max));
+        const expected = Array(200).fill(0).map(() => prng(min, max));
 
-        const observed = Array(5).fill(0).map(() => prng(min, max));
+        const observed = Array(200).fill(0).map(() => prng(min, max));
 
         const reduction = 1;
 
         const {probability} = chiSquaredTest(observed, expected, reduction);
         
-        expect(probability < 0.01).toEqual(true)
+        expect(probability < 0.01).toEqual(true);
         
     })
 })
